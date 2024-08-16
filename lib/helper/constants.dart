8d8ddcf4ff61d92b70922cec7c67986295291bd0
Yaya LY <yaya.ly@uadb.edu.sn>
2024-08-16 20:17:41 +0000

    Added enums to create the app's constant value.

---
+++ lib/helper/constants.dart
@@ -0,0 +1,31 @@
+enum DatabaseCollections{
+  users,
+  usersData,
+  reviews,
+}
+enum MenuItems{
+  add,
+  signOut
+}
+enum MenuItemsPhoto{
+  camera,
+  gallery
+}
+enum ReviewMode{
+  add,
+  edit,
+  readOnly
+}
+enum Affordability{
+  $,
+  $$,
+  $$$,
+  $$$$
+}
+enum ResponsiveSizes {
+  mobile(600.0),
+  webDesktopTable(601.0);
+
+  const ResponsiveSizes(this.value);
+  final double value;
+}
