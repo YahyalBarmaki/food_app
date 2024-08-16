d9267e203db2bcd3c0741b9beb4ae0216aaf8ce8
Yaya LY <yaya.ly@uadb.edu.sn>
2024-08-16 20:32:59 +0000

    Added page stubs for the review list, review grid photos and review map locations.

---
+++ lib/page/review_list/review_list.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ReviewList extends StatefulWidget {
+  const ReviewList({super.key});
+  static const route = "/review-list";
+  @override
+  State<ReviewList> createState() => _ReviewListState();
+}
+
+class _ReviewListState extends State<ReviewList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
