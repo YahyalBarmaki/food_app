1618fce2d3b8f514bf418dff481210d5feedd47e
Yaya LY <yaya.ly@uadb.edu.sn>
2024-08-17 21:57:33 +0000

    Added the Routes navigation helper class. Also added to home page the route name

---
+++ lib/helper/routes.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:food_app/page/authentication/user_forgot_password.dart';
+import 'package:food_app/page/authentication/user_login.dart';
+import 'package:food_app/page/authentication/user_register.dart';
+import 'package:food_app/page/home.dart';
+import 'package:food_app/page/review_entry/review_entry_edit.dart';
+import 'package:food_app/page/review_entry/review_entry_photo_zoom.dart';
+import 'package:food_app/page/review_entry/review_entry_view.dart';
+import 'package:food_app/page/review_grid_photos.dart';
+import 'package:food_app/page/review_list/review_list.dart';
+import 'package:food_app/page/review_map_locations/review_map_locations.dart';
+
+class Routes {
+  static final Map<String, WidgetBuilder> routes = {
+    Home.route: (BuildContext context) => const Home(),
+    UserLogin.route: (BuildContext context) => const UserLogin(),
+    UserRegister.route: (BuildContext context) => const UserRegister(),
+    UserForgotPassword.route: (BuildContext context) =>
+        const UserForgotPassword(),
+    ReviewList.route: (BuildContext context) => const ReviewList(),
+    ReviewEntryView.route: (BuildContext context) => const ReviewEntryView(),
+    ReviewEntryEdit.route: (BuildContext context) => const ReviewEntryEdit(),
+    ReviewEntryPhotoZoom.route: (BuildContext context) =>
+        const ReviewEntryPhotoZoom(),
+    ReviewMapLocations.route: (BuildContext context) =>
+        const ReviewMapLocations(),
+    ReviewGridPhotos.route: (BuildContext context) => const ReviewGridPhotos(),
+  };
+}
